e0c6c33da941eefb5d8c59ea6d8eea78e1ae5602
TheRadhe <rajveer202415@gmail.com>
2025-03-10 15:26:03 +0530

    Update buttons.py

--- NEXIOMUSIC/help/buttons.py
+++ NEXIOMUSIC/help/buttons.py
@@ -7,7 +7,7 @@ from NEXIOMUSIC import app
 class BUTTONS(object):
     MBUTTON = [
         [
-            InlineKeyboardButton("˹ᯓ𓆰𝅃꯭᳚ ⃪ ꯭꯭꯭꯭꯭꯭꯭꯭꯭꯭᪵᪳༎ ꯭⁢⁣⁤⁣⁣⁢⁣⁤⁢⁤⁣⁢⁤⁣⁤᪳᪳🇷꯭ ꯭𝐈‌𝛅꯭꯭ʜ꯭֟፝︢︣𝛖꯭ ꯭꯭🚩𝆺꯭𝅥༎ࠫ𐏓꯭ 𝅃 ˼", url="https://t.me/rishu1286")
+            InlineKeyboardButton("˹⎯፝֟፝֟⎯꯭᪵ ⃪ѕα͟ηα͟т͟α͟ηⷽιⷨ͟ 𝆺꯭𝅥⎯꯭🚩", url="https://t.me/FEELING_LIKE_KING")
         ],
         [
             InlineKeyboardButton("⌯ ʙᴧᴄᴋ ᴛσ ʜσϻє ⌯", callback_data="settingsback_helper"),
@@ -18,11 +18,11 @@ class BUTTONS(object):
     SBUTTON = [
  
         [
-            InlineKeyboardButton("ʀɪsʜυ ", url="https://t.me/ur_rishu_143"),
+            InlineKeyboardButton("ʙᴏᴛ-ᴄʜᴀᴍʙᴇʀ", url="https://t.me/ll_BOTCHAMBER_ll"),
         ],
         [
-            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/ur_support07"),
-            InlineKeyboardButton(" ᴜᴘᴅᴀᴛᴇ", url="https://t.me/vip_robotz"),
+            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/BOT_SUPPORT_GROUP7"),
+            InlineKeyboardButton(" ᴜᴘᴅᴀᴛᴇ", url="https://t.me/UR_RISHU_143"),
         ],
         [
             InlineKeyboardButton("ᴄʜᴧᴛ ɢᴄ", url="https://t.me/TheFriendsHeaven"),
